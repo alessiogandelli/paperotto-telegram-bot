3606cca9d52a8bab3b6c157adc9d72a2af08a5da
alessiogandelli <alessiogandelli99@gmail.com>
2021-04-04 02:16:58 +0200

    appizza

---
+++ bot.js
@@ -0,0 +1,32 @@
+
+const { Telegraf } = require('telegraf')
+const { exec } = require('child_process');
+
+const dotenv = require('dotenv');
+dotenv.config()
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.command('appizza', (ctx) => appizza(ctx))
+bot.command('ammazza', (ctx) => ammazza(ctx))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+function appizza(ctx){
+    exec('cd paperotto | java -Xmx2500M -Xms2500M -jar server.jar nogui', (err, stdout, stderr) => {
+        if (err) {
+          //some err occurred
+          ctx.retry(err)
+        } else {
+         // the *entire* stdout and stderr (buffered)
+         ctx.retry(`stdout: ${stdout}`);
+         ctx.retry(`stderr: ${stderr}`);
+        }
+      });
+}
+
+function ammazza(ctx){
+    ctx.reply('ammazza')
+}
