318c639d4f81dbe666f01f490b8b38caafb8ce1b
alessiogandelli <alessiogandelli99@gmail.com>
2021-04-04 03:34:18 +0200

    giomagi

--- bot.js
+++ bot.js
@@ -13,14 +13,20 @@ bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.command('appizza', (ctx) => appizza(ctx))
 bot.command('ammazza', (ctx) => ammazza(ctx))
+bot.command('giomagi',(ctx)=> giomagi(ctx))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
 
 function appizza(ctx){
-    shell.exec('/home/pi/dev/craft.sh')
+    shell.exec('/home/pi/dev/script/craft.sh')
     ctx.reply('forse ho appizzato')
 }
 
 function ammazza(ctx){
     ctx.reply('ammazza')
+}
+
+function giomagi(ctx){
+    shell.exec('/home/pi/dev/script/giomagi.sh')
+    ctx.reply('giomagi al lavoro')
 }
